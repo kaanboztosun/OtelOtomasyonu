8596280b24e757a18e3a0abde86b969f0ec861f6
kaanboztosun <63367114+kaanboztosun@users.noreply.github.com>
2021-01-01 21:52:52 +0300

    Add files via upload

---
+++ src/interfaces/Methods.java
@@ -0,0 +1,18 @@
+
+
+package interfaces;
+
+import javafx.event.ActionEvent;
+
+
+public interface Methods {
+
+    public void ekle(ActionEvent a);
+    public void sil();
+    public void guncelle(ActionEvent a);
+    public void clearform();
+    public void selectText();
+    public Object veriAll();
+    public void back(ActionEvent a);
+
+}
